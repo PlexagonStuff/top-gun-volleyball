bcd2954c3aca938e99020acdc318a2bfddc9639a
Nicholas-Stokes <plexagongb@gmail.com>
2024-11-15 13:29:59 -0500

    Revert "surely this will work?"
    This reverts commit 740d34f667f5a832376f4a9cd87060eafc814028.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import {Server} from 'socket.io'
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {path: '/.proxy', transports: ['websocket']});
+const io = new Server(server, {path: '/.proxy'});
 var rooms = [];
 var games = {}
 const __dirname = dirname(fileURLToPath(import.meta.url));
